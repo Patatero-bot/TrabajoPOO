d592636d88e88dd9bd75359fe304f68df38b71d9
elta01 <anthony.chandia@inacapmail.cl>
2025-11-14 12:08:11 -0300

    Add files via upload

---
+++ trabalotrabala/DTO/Registro_Version3.py
@@ -0,0 +1,29 @@
+from DAO.Conexion import ConexionBD
+
+class Registro:
+    def __init__(self, descripcion, horas_laborales, idpersona, idproyecto, fecha):
+        self.descripcion = descripcion
+        self.horas_laborales = horas_laborales
+        self.idpersona = idpersona
+        self.idproyecto = idproyecto
+        self.fecha = fecha
+
+    def guardar(self, conexion: ConexionBD):
+        cursor = conexion.ejecutar(
+            "INSERT INTO registros (descripcion, horas_laborales, idpersona, idproyecto, fecha) VALUES (%s, %s, %s, %s, %s)",
+            (self.descripcion, self.horas_laborales, self.idpersona, self.idproyecto, self.fecha)
+        )
+        print("Registro guardado." if cursor else "Error.")
+
+    @staticmethod
+    def listar_por_empleado(conexion: ConexionBD, idpersona):
+        cursor = conexion.ejecutar(
+            """
+            SELECT r.idregistros, r.fecha, r.descripcion, r.horas_laborales, p.nombre AS proyecto
+            FROM registros r
+            JOIN proyecto p ON r.idproyecto = p.idproyecto
+            WHERE r.idpersona = %s ORDER BY r.fecha DESC
+            """, 
+            (idpersona,)
+        )
+        return cursor.fetchall() if cursor else []
